835f2d133eba297adb538950e8ced6e91a8f6387
최민식 <guapito231@naver.com>
2025-07-06 00:15:30 +0900

    <update>(Main):메인페이지 activity, xml 구현/추천, 인기, 정책 리스트 예정

--- app/src/main/java/com/example/exitsw/MainActivity.kt
+++ app/src/main/java/com/example/exitsw/MainActivity.kt
@@ -7,5 +7,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // 처음 실행 시 Fragment를 동적으로 추가
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MainFragment())
+                .commit()
+        }
     }
 }
