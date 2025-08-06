989f626ce5e90d4e6574f104af3a148ae5137eff
최민식 <guapito231@naver.com>
2025-08-06 21:02:04 +0900

    <update>(Main):메인 페이지 상품 리스트

--- app/src/main/java/com/example/exitsw/MainFragment.kt
+++ app/src/main/java/com/example/exitsw/MainFragment.kt
@@ -5,6 +5,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainFragment : Fragment() {
         override fun onCreateView(
@@ -13,5 +15,19 @@ class MainFragment : Fragment() {
         ): View? {
         return inflater.inflate(R.layout.fragment_main, container, false)
         }
+
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+                super.onViewCreated(view, savedInstanceState)
+
+                // itemRecycler or recommendRecycler 뭐로 하지? 제어 흐름이 어떻게 되는지 회의 필요
+                val itemRecycler = view.findViewById<RecyclerView>(R.id.itemRecycler)
+
+                // LayoutManager 세팅 (this 대신 requireContext())
+                itemRecycler.layoutManager =
+                        LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
+
+                // Adapter 세팅
+                itemRecycler.adapter = RecommendAdapter(myRecommendList)
         }
+}
 
