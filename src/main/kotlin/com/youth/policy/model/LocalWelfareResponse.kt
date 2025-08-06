236398c65fb8ed73840a948c674eda1e4410c439
cjm0423 <87014584+cjm0423@users.noreply.github.com>
2025-08-06 14:16:31 +0900

    Update LocalWelfareResponse.kt

--- src/main/kotlin/com/youth/policy/model/LocalWelfareResponse.kt
+++ src/main/kotlin/com/youth/policy/model/LocalWelfareResponse.kt
@@ -1,36 +1,80 @@
 package com.youth.policy.model
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.fasterxml.jackson.dataformat.xml.annotation.*
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlRootElement
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlElementWrapper
+import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty
 
 @JacksonXmlRootElement(localName = "wantedList")
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class LocalWelfareListResponse(
-    @JacksonXmlProperty(localName = "totalCount") val totalCount: Int,
-    @JacksonXmlProperty(localName = "pageNo")     val pageNo: Int,
-    @JacksonXmlProperty(localName = "numOfRows")  val numOfRows: Int,
     @JacksonXmlProperty(localName = "resultCode")    val resultCode: String,
     @JacksonXmlProperty(localName = "resultMessage") val resultMessage: String,
+    @JacksonXmlProperty(localName = "numOfRows")     val numOfRows: Int,
+    @JacksonXmlProperty(localName = "pageNo")        val pageNo: Int,
+    @JacksonXmlProperty(localName = "totalCount")    val totalCount: Int,
     @JacksonXmlElementWrapper(useWrapping = false)
-    @JacksonXmlProperty(localName = "servList")
+    @JacksonXmlProperty(localName = "servList")       
     val servList: List<LocalWelfareService> = emptyList()
 )
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class LocalWelfareService(
-    @JacksonXmlProperty(localName = "servId")           val servId: String,
-    @JacksonXmlProperty(localName = "servNm")           val servNm: String,
-    @JacksonXmlProperty(localName = "jrsdInsttNm")      val jrsdInsttNm: String?,
-    @JacksonXmlProperty(localName = "lifeArray")        val lifeArray: String?,
-    @JacksonXmlProperty(localName = "trgterIndvdlArray")val trgterIndvdlArray: String?,
-    @JacksonXmlProperty(localName = "intrsThemaArray")  val intrsThemaArray: String?
+    @JacksonXmlProperty(localName = "bizChrDeptNm")          val bizChrDeptNm: String?,
+    @JacksonXmlProperty(localName = "ctpvNm")                val ctpvNm: String?,
+    @JacksonXmlProperty(localName = "sggNm")                 val sggNm: String?,
+    @JacksonXmlProperty(localName = "servDgst")              val servDgst: String?,
+    @JacksonXmlProperty(localName = "servDtlLink")           val servDtlLink: String?,
+    @JacksonXmlProperty(localName = "lifeNmArray")           val lifeNmArray: String?,
+    @JacksonXmlProperty(localName = "intrsThemaNmArray")     val intrsThemaNmArray: String?,
+    @JacksonXmlProperty(localName = "sprtCycNm")             val sprtCycNm: String?,
+    @JacksonXmlProperty(localName = "srvPvsnNm")             val srvPvsnNm: String?,
+    @JacksonXmlProperty(localName = "aplyMtdNm")             val aplyMtdNm: String?,
+    @JacksonXmlProperty(localName = "inqNum")                val inqNum: String?,
+    @JacksonXmlProperty(localName = "lastModYmd")           val lastModYmd: String?,
+    @JacksonXmlProperty(localName = "servId")                val servId: String,
+    @JacksonXmlProperty(localName = "servNm")                val servNm: String,
+    @JacksonXmlProperty(localName = "trgterIndvdlNmArray")   val trgterIndvdlNmArray: String?
 )
 
 @JacksonXmlRootElement(localName = "wantedDtl")
 @JsonIgnoreProperties(ignoreUnknown = true)
 data class LocalWelfareDetailResponse(
-    @JacksonXmlProperty(localName = "servId")    val servId: String,
-    @JacksonXmlProperty(localName = "servNm")    val servNm: String,
-    @JacksonXmlProperty(localName = "tgtrDtlCn") val tgtrDtlCn: String?,
-    @JacksonXmlProperty(localName = "srvPvsnNm") val srvPvsnNm: String?
+    @JacksonXmlProperty(localName = "resultCode")           val resultCode: String,
+    @JacksonXmlProperty(localName = "resultMessage")        val resultMessage: String,
+    @JacksonXmlProperty(localName = "servId")               val servId: String,
+    @JacksonXmlProperty(localName = "servNm")               val servNm: String,
+    @JacksonXmlProperty(localName = "enfcBgngYmd")          val enfcBgngYmd: String?,
+    @JacksonXmlProperty(localName = "enfcEndYmd")           val enfcEndYmd: String?,
+    @JacksonXmlProperty(localName = "bizChrDeptNm")         val bizChrDeptNm: String?,
+    @JacksonXmlProperty(localName = "ctpvNm")               val ctpvNm: String?,
+    @JacksonXmlProperty(localName = "sggNm")                val sggNm: String?,
+    @JacksonXmlProperty(localName = "servDgst")             val servDgst: String?,
+    @JacksonXmlProperty(localName = "lifeNmArray")          val lifeNmArray: String?,
+    @JacksonXmlProperty(localName = "trgterIndvdlNmArray")  val trgterIndvdlNmArray: String?,
+    @JacksonXmlProperty(localName = "intrsThemaNmArray")    val intrsThemaNmArray: String?,
+    @JacksonXmlProperty(localName = "sprtCycNm")            val sprtCycNm: String?,
+    @JacksonXmlProperty(localName = "srvPvsnNm")            val srvPvsnNm: String?,
+    @JacksonXmlProperty(localName = "aplyMtdNm")            val aplyMtdNm: String?,
+    @JacksonXmlProperty(localName = "sprtTrgtCn")           val sprtTrgtCn: String?,
+    @JacksonXmlProperty(localName = "slctCritCn")           val slctCritCn: String?,
+    @JacksonXmlProperty(localName = "alwServCn")            val alwServCn: String?,
+    @JacksonXmlProperty(localName = "aplyMtdCn")            val aplyMtdCn: String?,
+    @JacksonXmlProperty(localName = "inqNum")               val inqNum: String?,
+    @JacksonXmlProperty(localName = "lastModYmd")          val lastModYmd: String?,
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "inqplCtadrList")       val inqplCtadrList: List<WelfareInfo> = emptyList(),
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "inqplHmpgReldList")    val inqplHmpgReldList: List<WelfareInfo> = emptyList(),
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "baslawList")          val baslawList: List<WelfareInfo> = emptyList(),
+    @JacksonXmlElementWrapper(useWrapping = false)
+    @JacksonXmlProperty(localName = "basfrmList")          val basfrmList: List<WelfareInfo> = emptyList()
+)
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+data class WelfareInfo(
+    @JacksonXmlProperty(localName = "wlfareInfoDtlCd")    val wlfareInfoDtlCd: String?,
+    @JacksonXmlProperty(localName = "wlfareInfoReldNm")   val wlfareInfoReldNm: String?,
+    @JacksonXmlProperty(localName = "wlfareInfoReldCn")   val wlfareInfoReldCn: String?
 )
